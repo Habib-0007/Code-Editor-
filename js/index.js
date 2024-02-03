b2398f1fc2a95e04627dedf5ee584e84ff73c19f
Habib-0007 <adebayohabib7@gmail.com>
2024-02-04 00:37:46 +0100

    new changes to the Editor UI

--- js/index.js
+++ js/index.js
@@ -8,28 +8,40 @@ var cssCont = document.querySelector(".css code");
 var jsCont = document.querySelector(".js code");
 
 document.getElementById("main").style.height = `${window.innerHeight}px`;
-htmlCont.textContent =
-	'<!DOCTYPE html><html lang="en"><head> <meta charset="UTF-8" /><meta name="viewport"content="width=device-width, initial-scale=1.0" /><meta http-equiv="X-UA-Compatible" content="ie=edge"/><title>Document</title></head><body></body></html>';
-
-var jsScript = document.getElementById("js-script");
 
 header.addEventListener("click", e => {
 	if (e.target.classList.contains("htmlBtn")) {
+		document.querySelector(".htmlBtn").classList.add("active");
+		document.querySelector(".cssBtn").classList.remove("active");
+		document.querySelector(".jsBtn").classList.remove("active");
+		document.querySelector(".outputBtn").classList.remove("active");
 		html.style.display = "block";
 		css.style.display = "none";
 		js.style.display = "none";
 		output.style.display = "none";
 	} else if (e.target.classList.contains("cssBtn")) {
+		document.querySelector(".htmlBtn").classList.remove("active");
+		document.querySelector(".cssBtn").classList.add("active");
+		document.querySelector(".jsBtn").classList.remove("active");
+		document.querySelector(".outputBtn").classList.remove("active");
 		html.style.display = "none";
 		css.style.display = "block";
 		js.style.display = "none";
 		output.style.display = "none";
 	} else if (e.target.classList.contains("jsBtn")) {
+	  document.querySelector(".htmlBtn").classList.remove("active");
+		document.querySelector(".cssBtn").classList.remove("active");
+		document.querySelector(".jsBtn").classList.add("active");
+		document.querySelector(".outputBtn").classList.remove("active");
 		html.style.display = "none";
 		css.style.display = "none";
 		js.style.display = "block";
 		output.style.display = "none";
 	} else if (e.target.classList.contains("outputBtn")) {
+		document.querySelector(".htmlBtn").classList.remove("active");
+		document.querySelector(".cssBtn").classList.remove("active");
+		document.querySelector(".jsBtn").classList.remove("active");
+		document.querySelector(".outputBtn").classList.add("active");
 		html.style.display = "none";
 		css.style.display = "none";
 		js.style.display = "none";
@@ -43,10 +55,16 @@ function runCode() {
 	localStorage.setItem("css_code", cssCont.textContent);
 	localStorage.setItem("js_code", jsCont.textContent);
 
-	output.innerHTML =
-		`<style>${localStorage.css_code}</style>` + localStorage.html_code;
+	outputCode =
+		`<style>${localStorage.css_code}</style>` +
+		localStorage.html_code +
+		`<script defer>${localStorage.js_code}</script>`;
 
-	jsScript.textContent = `${localStorage.js_code}`;
+	var iframeDoc = output.contentWindow.document;
+
+	iframeDoc.open();
+	iframeDoc.write(outputCode);
+	iframeDoc.close();
 }
 
 htmlCont.textContent = localStorage.html_code;
